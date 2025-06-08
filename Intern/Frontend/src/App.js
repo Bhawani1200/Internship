cc96e6bab00c894498670abd8cfbc61a0c6e7601
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-08 20:45:20 +0545

    first commit

---
+++ Intern/Frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navigation from "./customer/components/Navigation/Navigation.jsx";
+// import HomePage from "./customer/pages/HomePage/HomePage";
+import Footer from "./customer/components/Footer/Footer";
+import Product from "./customer/components/product/Product.jsx";
+// import Product from "../src/customer/components/Product/Product.jsx";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <div>
+        {/* <HomePage /> */}
+        <Product/>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
