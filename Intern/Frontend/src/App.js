53c6e6e1f1773b2275d633881ea077b8c068ff12
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-09 16:43:04 +0545

    singleprofuct page

--- Intern/Frontend/src/App.js
+++ Intern/Frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Navigation from "./customer/components/Navigation/Navigation.jsx";
-// import HomePage from "./customer/pages/HomePage/HomePage";
+import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
 import Product from "./customer/components/product/Product.jsx";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails.jsx";
 // import Product from "../src/customer/components/Product/Product.jsx";
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product/>
+        {/* <Product/> */}
+        <ProductDetails/>
       </div>
       <Footer/>
     </div>
